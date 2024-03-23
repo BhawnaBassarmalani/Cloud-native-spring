8becac81c456cf020df43b401608c82ab37b005e
BhawnaBassarmalani <bassarmalani.b@northeastern.edu>
2024-03-23 12:14:36 -0400

    Inmemoryrepository updation

--- src/main/java/com/polarbookshop/catalogservice/persistence/InMemoryBookRepository.java
+++ src/main/java/com/polarbookshop/catalogservice/persistence/InMemoryBookRepository.java
@@ -1,40 +1,44 @@
-package com.polarbookshop.catalogservice.persistence;
 
-import com.polarbookshop.catalogservice.domain.Book;
-import com.polarbookshop.catalogservice.domain.BookRepository;
-import org.springframework.stereotype.Repository;
+package com.polarbookshop.catalogservice.persistence;
 
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
+import com.polarbookshop.catalogservice.domain.Book;
+import com.polarbookshop.catalogservice.domain.BookRepository;
+
+import org.springframework.stereotype.Repository;
+
 @Repository
 public class InMemoryBookRepository implements BookRepository {
 
-    private static final Map<String,Book> books = new ConcurrentHashMap<>();
+    private static final Map<String, Book> books = new ConcurrentHashMap<>();
 
     @Override
     public Iterable<Book> findAll() {
-        return null;
+        return books.values();
     }
 
     @Override
     public Optional<Book> findByIsbn(String isbn) {
-        return Optional.empty();
+        return existsByIsbn(isbn) ? Optional.of(books.get(isbn)) : Optional.empty();
     }
 
     @Override
     public boolean existsByIsbn(String isbn) {
-        return false;
+        return books.get(isbn) != null;
     }
 
     @Override
     public Book save(Book book) {
-        return null;
+        books.put(book.isbn(), book);
+        return book;
     }
 
     @Override
     public void deleteByIsbn(String isbn) {
-
+        books.remove(isbn);
     }
+
 }
