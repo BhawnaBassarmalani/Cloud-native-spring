b76e550b55293b742b55b30f34460cfce56f9cda
BhawnaBassarmalani <bassarmalani.b@northeastern.edu>
2024-03-24 23:22:59 -0400

    defining profiles

--- src/main/java/com/polarbookshop/catalogservice/HomeController.java
+++ src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -1,14 +1,21 @@
 package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.config.PolarProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+    private final PolarProperties polarProperties;
+
+    public HomeController(PolarProperties polarProperties){
+        this.polarProperties = polarProperties;
+    }
    // docker run --rm --name catalog-service -p 8080:8080 catalog-service:0.0.1-SNAPSHOT
     //kubectl create deployment catalog-service --image=catalog-service:0.0.1-SNAPSHOT
     @GetMapping("/")
     public String getGreeting(){
-        return "Welcome to the book catalog";
+
+        return polarProperties.getGreeting();
     }
 }
