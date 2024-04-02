2f0ceca7b7f1b84dd60ce5a4bda5994c8737c1ab
BhawnaBassarmalani <bassarmalani.b@northeastern.edu>
2024-04-02 08:46:17 -0400

    Configuring the connection to database using JDBC excluding

--- src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
+++ src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
@@ -2,11 +2,12 @@ package com.polarbookshop.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 
 @ConfigurationPropertiesScan
-@SpringBootApplication
+@SpringBootApplication(exclude = {JdbcTemplateAutoConfiguration.class})
 public class CatalogServiceApplication {
 
 	public static void main(String[] args) {
